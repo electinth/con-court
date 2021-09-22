6dc3c0d99c62150b171d3e0c7444340d198b944d
MEEP00H <MEEP00H@users.noreply.github.com>
2021-09-22 05:03:33 +0000

    deploy: 522a43c1f9b0148958dc641db9f31f9c38d9c19b

---
+++ _nuxt/a4666c9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{451:function(n,t,e){"use strict";e.r(t);e(60),e(236);t.default=function(n){var t=n.split("-"),e=parseInt(t[0],10),o=t[1],r=t[2],c=new Date(r,o,e).toLocaleDateString("th-TH",{year:"numeric",month:"long",day:"numeric"}).split(" ");return c[0]+" "+c[1]}}}]);
