0d298406b655231354a8b0abf199a76bf17a0d51
Pooh <sophon@punchup.world>
2021-10-12 11:54:47 +0700

    fixed wrong date

--- components/util/FormatDate.js
+++ components/util/FormatDate.js
@@ -2,7 +2,7 @@ function FormatDate(date) {
 
     const date_list = date.split('-')
     const day = parseInt(date_list[0], 10)
-    const month = date_list[1]
+    const month = date_list[1] - 1
     const year = date_list[2]
     const format_date = new Date(year, month, day)
     const result = format_date.toLocaleDateString('th-TH', {
